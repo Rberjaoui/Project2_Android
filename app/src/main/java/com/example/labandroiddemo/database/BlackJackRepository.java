de264e7aaf65658d1d59988515caf7c2285f200d
aTrujillo4321 <aletrujillo@csumb.edu>
2025-11-08 19:04:23 -0800

    Did the repository. Either it is finished, or needs some touches.

--- app/src/main/java/com/example/labandroiddemo/database/BlackJackRepository.java
+++ app/src/main/java/com/example/labandroiddemo/database/BlackJackRepository.java
@@ -2,6 +2,10 @@ package com.example.labandroiddemo.database;
 
 import android.app.Application;
 
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class BlackJackRepository {
     private final BlackJackDAO blackJackDAO;
     private final UserDAO userDAO;
@@ -22,5 +26,29 @@ public class BlackJackRepository {
             }
         }
         return repository;
+    }
+
+    public void insertGymLog(BlackJack blackJack) {
+        BlackJackDatabase.databaseWriteExecutor.execute(() -> {
+            blackJackDAO.insert(blackJack);
+        });
+    }
+
+    public void insertUser(User... user) {
+        BlackJackDatabase.databaseWriteExecutor.execute(() -> {
+            userDAO.insert(user);
+        });
+    }
+
+    public LiveData<User> getUserByUserName(String username) {
+        return userDAO.getUserByUserName(username);
+    }
+
+    public LiveData<User> getUserByUserId(int userId) {
+        return userDAO.getUserByUserId(userId);
+    }
+
+    public LiveData<List<BlackJack>> getAllLogsByUserIdLiveData(int loggedInUserId) {
+        return blackJackDAO.getRecordSetUserIdLiveData(loggedInUserId);
     }
 }
