777e03ad5820cf0c7f36da6e4da78f62fbc92aac
aTrujillo4321 <aletrujillo@csumb.edu>
2025-11-11 16:51:52 -0800

    Did the UserDAO.

---
+++ app/src/main/java/com/example/labandroiddemo/database/UserDAO.java
@@ -0,0 +1,36 @@
+package com.example.labandroiddemo.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.labandroiddemo.database.entities.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(User... user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("SELECT * FROM " + BlackJackDatabase.USER_TABLE + " ORDER BY username")
+    LiveData<List<User>> getAllUsers();
+
+    @Query("DELETE FROM " + BlackJackDatabase.USER_TABLE)
+    void deleteAll();
+
+    @Query("SELECT * FROM " + BlackJackDatabase.USER_TABLE + " WHERE username == :username ")
+    LiveData<User> getUserByUserName(String username);
+
+    @Query("SELECT * FROM " + BlackJackDatabase.USER_TABLE + " WHERE id == :userId ")
+    LiveData<User> getUserByUserId(int userId);
+
+    @Query("SELECT * FROM " + BlackJackDatabase.USER_TABLE + " WHERE username == :username ")
+    User getUserByUserName_TEST(String username);
+}
---
+++ app/src/main/java/com/example/labandroiddemo/database/entities/User.java
@@ -0,0 +1,7 @@
+package com.example.labandroiddemo.database.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+public class User {
+}
