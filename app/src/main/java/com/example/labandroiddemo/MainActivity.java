7b7d51ff587fd3da508ac0eee3d130828b5b12d6
viri <viridianaramos37@gmail.com>
2025-11-13 12:43:59 -0800

    created another branch to work on LandingPage

--- app/src/main/java/com/example/labandroiddemo/MainActivity.java
+++ app/src/main/java/com/example/labandroiddemo/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.labandroiddemo;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.labandroiddemo.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
+
+    private JackRushViewModel  jackRushViewModel;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        jackRushViewModel = new ViewModelProvider(this).get(jackRushViewModel.class);
 
 
 
