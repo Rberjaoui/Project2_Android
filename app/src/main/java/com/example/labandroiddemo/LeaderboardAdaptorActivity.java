299d3529b598406032f3815fff8617b2529a1f5f
aTrujillo4321 <aletrujillo@csumb.edu>
2025-11-17 13:57:06 -0800

    Did leaderboardAdapter

---
+++ app/src/main/java/com/example/labandroiddemo/LeaderboardAdaptorActivity.java
@@ -0,0 +1,50 @@
+package com.example.labandroiddemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.labandroiddemo.database.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.UserViewHolder> {
+    private List<User> users = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.leaderboard_item, parent, false);
+        return new UserViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User currentUser = users.get(position);
+        holder.usernameTextView.setText(currentUser.getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+        notifyDataSetChanged();
+    }
+
+    static class UserViewHolder extends RecyclerView.ViewHolder {
+        private final TextView usernameTextView;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usernameTextView = itemView.findViewById(R.id.usernameTextView);
+        }
+    }
+}
